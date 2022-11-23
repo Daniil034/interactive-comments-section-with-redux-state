cb0e1a6a3c52dcd2b769724d7df91224b50eaa9f
21 112 <zyabin@yandex.ru>
2022-11-23 12:51:39 +0300

    edit images paths

--- src/components/CommentHeader/CommentHeader.jsx
+++ src/components/CommentHeader/CommentHeader.jsx
@@ -7,17 +7,25 @@ export const CommentHeader = ({
   createdAt,
   currentUser,
 }) => {
-  const userImagePNG = new URL(`../../data/images/avatars/${userImage.png.slice(17)}`, import.meta.url).href;
+  const userImagePNG = new URL(
+    `../../data/images/avatars/${userImage.png.slice(17)}`,
+    import.meta.url
+  ).href;
+  console.log(userImage.png);
   const userImageWEBP = useRef();
-  const onImageError = () =>
-    (userImageWEBP.current.src = `./src/data/images/avatars/${userImage.webp.slice(17)}`);
+  const onImageError = () => {
+    userImageWEBP.current.src = new URL(
+      `../../data/images/avatars/${userImage.webp.slice(17)}`,
+      import.meta.url
+    ).href;
+  };
   const you = <p className="comment-header__you">you</p>;
   return (
     <div className="comment-header">
       <div className="comment-header__img">
         <img
           ref={userImageWEBP}
-          src={`src/data/${userImagePNG.slice(2)}`}
+          src={userImagePNG}
           alt="user avatar"
           onError={onImageError}
         />
